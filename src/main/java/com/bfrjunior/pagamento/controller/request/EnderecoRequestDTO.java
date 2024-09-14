745e41d35296d4a252b8a4a1a7cc222e46570d20
Fernando Rocha <beneditofernandor@gmail.com>
2024-09-14 18:09:22 -0300

    feat:create DTO's

---
+++ src/main/java/com/bfrjunior/pagamento/controller/request/EnderecoRequestDTO.java
@@ -0,0 +1,21 @@
+package com.bfrjunior.pagamento.controller.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EnderecoRequestDTO {
+
+    private String rua;
+
+    private Long numero;
+
+    private String bairro;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String cep;
+}
